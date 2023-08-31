6d655f627a1533dd16304c2a66df541ce2bc8916
otvertka <otvertka123@ukr.net>
2023-08-31 16:11:30 +0300

    useState

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 function PostList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Jeka" body="React is awesome!" />
-      <Post author="Yevhen" body="JS is amazing!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Jeka" body="React is awesome!" />
+        <Post author="Yevhen" body="JS is amazing!" />
+      </ul>
+    </>
   );
 }
 
